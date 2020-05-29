8bd8dc59aa3f84bf654c6fe02bb8d53c9d0fa12f
daniel521221 <60629933+daniel521221@users.noreply.github.com>
2020-05-29 16:24:30 -0700

    changes in index.html and User class

---
+++ EventosTec/EventosTec.Web/Models/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventosTec.Web.Models.Entities
+{
+    public class User:IdentityUser
+    {
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
